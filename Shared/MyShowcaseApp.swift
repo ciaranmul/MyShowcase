00134a2a8b90f547fb425e5b7bb3f082399bdbb0
Ciarán Mulholland <ciaran@slicelife.com>
2021-10-07 20:57:14 +0100

    Initial Commit

---
+++ Shared/MyShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyShowcaseApp.swift
+//  Shared
+//
+//  Created by Ciarán Mulholland on 07/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct MyShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
